d810c04bde8a9430320607da538405130d6c91c0
Kevin Later <laterkevin@icloud.com>
2022-06-22 12:56:18 +0200

    Aufgabe bearbeitet!

--- app/src/main/java/com/example/speisekarteapp/MainActivity.kt
+++ app/src/main/java/com/example/speisekarteapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.speisekarteapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.speisekarteapp.databinding.ActivityMainBinding
 import kotlin.math.round
 
 /**
@@ -24,18 +26,52 @@ class MainActivity : AppCompatActivity() {
 
         // Lege hier die Binding Variable an
         // TODO Schreibe hier deinen Code
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         // Setze hier die Namen, Preise und Count in der View über die binding Variable
         // TODO Schreibe hier deinen Code
+        binding.drink1Name.text = drink1.name
+        binding.drink2Name.text = drink2.name
+        binding.drink3Name.text = drink3.name
+
+        binding.drink1Price.text = drink1.price.toString()
+        binding.drink2Price.text = drink2.price.toString()
+        binding.drink3Price.text = drink3.price.toString()
+
+        binding.drink1Count.text = drink1.count.toString()
+        binding.drink2Count.text = drink2.count.toString()
+        binding.drink3Count.text = drink3.count.toString()
 
         // implementiere einen onClicklistener für das Kaffee Icon
         // TODO Schreibe hier deinen Code
+        binding.drink1Button.setOnClickListener {
+            addToBill(drink1.price)
+            drink1.count++
+            binding.drink1Count.text = drink1.count.toString()
+            var deciStellen = "%.2f".format(bill)
+            binding.totalPrice.text = deciStellen
+        }
 
         // implementiere einen onClicklistener für das Wein Icon
         // TODO Schreibe hier deinen Code
+        binding.drink2Button.setOnClickListener {
+            addToBill(drink2.price)
+            drink2.count++
+            binding.drink2Count.text = drink2.count.toString()
+            var deciStellen = "%.2f".format(bill)
+            binding.totalPrice.text = deciStellen
+        }
 
         // implementiere einen onClicklistener für das Cocktail Icon
         // TODO Schreibe hier deinen Code
+        binding.drink3Button.setOnClickListener {
+            addToBill(drink1.price)
+            drink3.count++
+            binding.drink3Count.text = drink3.count.toString()
+            var deciStellen = "%.2f".format(bill)
+            binding.totalPrice.text = deciStellen
+        }
     }
 
     /**
